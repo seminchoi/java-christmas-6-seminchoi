851a31fe7acc5652a5b07a987bce87349fc0a18f
seminchoi <choicco89@gmail.com>
2023-11-10 19:09:11 +0900

    refactor(model.OrderMenu) : 추상 클래스로 변경

--- src/main/java/christmas/model/OrderMenu.java
+++ src/main/java/christmas/model/OrderMenu.java
@@ -2,8 +2,9 @@ package christmas.model;
 
 import java.util.EnumMap;
 
-public class OrderMenu {
-    private final EnumMap<Menu, Integer> orderMenus = new EnumMap<>(Menu.class);
+public abstract class OrderMenu {
+
+    protected final EnumMap<Menu, Integer> orderMenus = new EnumMap<>(Menu.class);
 
     public void addOrder(Menu menu, int count) {
         orderMenus.put(menu, count);
