0966ab4746e774c30b98440e8f553937602b870a
seminchoi <choicco89@gmail.com>
2023-11-10 19:10:44 +0900

    feat(model.ClientOrderMenu) : 최대 주문 수량 검증 기능 구현

--- src/main/java/christmas/model/ClientOrderMenu.java
+++ src/main/java/christmas/model/ClientOrderMenu.java
@@ -3,4 +3,22 @@ package christmas.model;
 import christmas.message.ErrorMessage;
 
 public class ClientOrderMenu extends OrderMenu {
+    private final static int MAX_COUNT = 20;
+
+    public void validate() {
+        int totalCount = 0;
+        for (Integer count : super.orderMenus.values()) {
+            totalCount += count;
+        }
+    }
+
+    public void validateTotalCount(int totalCount) {
+        if (isOverMaxCount(totalCount)) {
+            throw new IllegalArgumentException(ErrorMessage.TOO_MANY_ORDERS.getMessage());
+        }
+    }
+
+    private boolean isOverMaxCount(int totalCount) {
+        return totalCount > MAX_COUNT;
+    }
 }
