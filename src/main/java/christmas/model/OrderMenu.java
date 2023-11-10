f267c399002cf870dea846a829ecc6e60c941acd
seminchoi <choicco89@gmail.com>
2023-11-10 18:49:57 +0900

    feat(model.OrderMenu) : 주문을 추가하는 기능 구현

--- src/main/java/christmas/model/OrderMenu.java
+++ src/main/java/christmas/model/OrderMenu.java
@@ -4,4 +4,8 @@ import java.util.EnumMap;
 
 public class OrderMenu {
     private final EnumMap<Menu, Integer> orderMenus = new EnumMap<>(Menu.class);
+
+    public void addOrder(Menu menu, int count) {
+        orderMenus.put(menu, count);
+    }
 }
