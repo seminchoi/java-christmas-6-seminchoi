449fe993802916715a5c6d1288865aa4e6cde046
seminchoi <choicco89@gmail.com>
2023-11-10 18:13:14 +0900

    feat(model.DecemberDate) : DecemberDate 클래스 생성 및 유효성 검사, 생성자 구현

--- src/main/java/christmas/message/ErrorMessage.java
+++ src/main/java/christmas/message/ErrorMessage.java
@@ -2,6 +2,7 @@ package christmas.message;
 
 public enum ErrorMessage {
     PREFIX("[ERROR] %s"),
+    OUT_OF_RANGE_DATE("유효하지 않은 날짜입니다. 다시 입력해 주세요.")
     ;
     private final String message;
 
---
+++ src/main/java/christmas/model/DecemberDate.java
@@ -0,0 +1,27 @@
+package christmas.model;
+
+import christmas.message.ErrorMessage;
+import java.time.LocalDate;
+
+public class DecemberDate {
+    private static final int YEAR = 2023;
+    private static final int MONTH = 12;
+    private static final int MIN_DATE = 1;
+    private static final int MAX_DATE = 31;
+    private final LocalDate date;
+
+    public DecemberDate(int date) {
+        validate(date);
+        this.date = LocalDate.of(YEAR, MONTH, date);
+    }
+
+    private void validate(int date) {
+        if (isOutOfRangeDate(date)) {
+            throw new IllegalArgumentException(ErrorMessage.OUT_OF_RANGE_DATE.getMessage());
+        }
+    }
+
+    private boolean isOutOfRangeDate(int date) {
+        return date < MIN_DATE || date > MAX_DATE;
+    }
+}
