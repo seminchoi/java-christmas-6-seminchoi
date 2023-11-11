fe8ad174e0c68861cdcdbc5038a089759d7eb25a
seminchoi <choicco89@gmail.com>
2023-11-11 11:14:27 +0900

    feat(model.EventType) : EventType 상수 세분화

--- src/main/java/christmas/model/EventType.java
+++ src/main/java/christmas/model/EventType.java
@@ -1,6 +1,19 @@
 package christmas.model;
 
 public enum EventType {
-    DISCOUNT,
-    MENU_GIFT
+    CHRISTMAS_DISCOUNT("크리스마스 디데이 할인"),
+    WEEK_DAY_DISCOUNT("평일 할인"),
+    WEEKEND_DISCOUNT("주말 할인"),
+    SPECIAL_DISCOUNT("특별 할인"),
+    MENU_GIFT("증정 이벤트");
+
+    private final String description;
+
+    EventType(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
