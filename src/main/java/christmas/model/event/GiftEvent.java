4975da5ad8ec9071ad2f3f9bbb24ca92f3a3d984
seminchoi <choicco89@gmail.com>
2023-11-11 16:43:41 +0900

    feat(domain.event.GiftEvent) : 증정 이벤트 인터페이스 정의

---
+++ src/main/java/christmas/model/event/GiftEvent.java
@@ -0,0 +1,8 @@
+package christmas.model.event;
+
+import christmas.model.ClientOrders;
+import christmas.model.GiftOrders;
+
+public interface GiftEvent extends Event {
+    GiftOrders apply(ClientOrders clientOrders);
+}
