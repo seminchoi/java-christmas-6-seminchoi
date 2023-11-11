37f07bb73b6f8e92472768e303468110c5944d01
seminchoi <choicco89@gmail.com>
2023-11-11 17:17:56 +0900

    feat(model.event.ChampagneGiftEvent) : 샴페인을 증정하는 이벤트 기능 구현

---
+++ src/main/java/christmas/model/event/ChampagneGiftEvent.java
@@ -0,0 +1,32 @@
+package christmas.model.event;
+
+import static christmas.model.Menu.CHAMPAGNE;
+import static christmas.model.event.EventType.CHAMPAGNE_GIFT;
+
+import christmas.model.ClientOrders;
+import christmas.model.GiftOrders;
+import christmas.model.Menu;
+
+public class ChampagneGiftEvent implements GiftEvent {
+    private static final EventType type = CHAMPAGNE_GIFT;
+    private static final int MIN_ORDER_AMOUNT_FOR_SATISFIED_CONDITION = 120_000;
+    private static final Menu GIFT_MENU = CHAMPAGNE;
+    private static final int GIFT_COUNT = 1;
+
+    @Override
+    public GiftOrders apply(GiftOrders giftOrders, ClientOrders clientOrders) {
+        if(isSatisfiedEventConditions(clientOrders)) {
+            clientOrders.addOrder(GIFT_MENU, GIFT_COUNT);
+        }
+        return giftOrders;
+    }
+
+    private boolean isSatisfiedEventConditions(ClientOrders clientOrders) {
+        return clientOrders.sumTotalAmount() >= MIN_ORDER_AMOUNT_FOR_SATISFIED_CONDITION;
+    }
+
+    @Override
+    public EventType getType() {
+        return type;
+    }
+}
