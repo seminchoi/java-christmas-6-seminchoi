9c733597d5de6d6ad6f61e012156c665ef73797b
seminchoi <choicco89@gmail.com>
2023-11-11 11:30:41 +0900

    feat(model.event.EventType) : EventClassification 필드 추가

---
+++ src/main/java/christmas/model/event/EventType.java
@@ -0,0 +1,33 @@
+package christmas.model.event;
+
+import static christmas.model.event.EventClassification.DISCOUNT;
+import static christmas.model.event.EventClassification.MENU_GIFT;
+import static christmas.model.event.EventClassification.PERFORMANCE;
+
+public enum EventType {
+    CHRISTMAS_DISCOUNT(DISCOUNT, "크리스마스 디데이 할인"),
+    WEEK_DAY_DISCOUNT(DISCOUNT, "평일 할인"),
+    WEEKEND_DISCOUNT(DISCOUNT, "주말 할인"),
+    SPECIAL_DISCOUNT(DISCOUNT, "특별 할인"),
+
+    CHAMPAGNE_GIFT(MENU_GIFT, "증정 이벤트"),
+
+    DECEMBER_BADGE(PERFORMANCE, "12월 이벤트 배지")
+    ;
+
+    private final EventClassification classification;
+    private final String description;
+
+    EventType(EventClassification classification, String description) {
+        this.classification = classification;
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public EventClassification getClassification() {
+        return classification;
+    }
+}
