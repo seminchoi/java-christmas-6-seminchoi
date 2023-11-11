ed306204733ac0c4d29c000fbb092e4df1195ce6
seminchoi <choicco89@gmail.com>
2023-11-11 14:07:54 +0900

    refactor(domain.event.WeekDayDiscountEvent) : 추상 클래스를 상속받아 구현하도록 변경

--- src/main/java/christmas/model/event/WeekDayDiscountEvent.java
+++ src/main/java/christmas/model/event/WeekDayDiscountEvent.java
@@ -3,44 +3,27 @@ package christmas.model.event;
 import static christmas.model.MenuCategory.DESSERT;
 import static christmas.model.event.EventType.WEEK_DAY_DISCOUNT;
 
-import christmas.model.ClientOrders;
 import christmas.model.Menu;
+import christmas.model.MenuCategory;
 import christmas.model.calendar.DecemberDate;
 
-public class WeekDayDiscountEvent implements DiscountEvent {
+public class WeekDayDiscountEvent extends AbstractTargetDiscountEvent<MenuCategory> {
     private final static EventType type = WEEK_DAY_DISCOUNT;
     private final static int COUNT_PER_DISCOUNT_AMOUNT = 2023;
+    private final static MenuCategory TARGET_CATEGORY = DESSERT;
 
-    @Override
-    public int apply(final DecemberDate decemberDate, final ClientOrders orderMenu) {
-        if(isEventActive(decemberDate)) {
-            return calculateDiscountAmount(orderMenu);
-        }
-        return 0;
+    public WeekDayDiscountEvent() {
+        super(COUNT_PER_DISCOUNT_AMOUNT, TARGET_CATEGORY);
     }
 
-    private boolean isEventActive(final DecemberDate decemberDate) {
+    @Override
+    boolean isEventActive(DecemberDate decemberDate) {
         return decemberDate.isWeekDay();
     }
 
-    private int calculateDiscountAmount(final ClientOrders orderMenu) {
-        return sumTargetMenuCount(orderMenu) * COUNT_PER_DISCOUNT_AMOUNT;
-    }
-
-    private int sumTargetMenuCount(final ClientOrders orderMenu) {
-        int targetMenusCount = 0;
-
-        for (Menu menu : orderMenu) {
-            if(isTargetMenu(menu)) {
-                targetMenusCount += orderMenu.getCountByMenu(menu);
-            }
-        }
-
-        return targetMenusCount;
-    }
-
-    private boolean isTargetMenu(Menu menu) {
-        return menu.getCategory() == DESSERT;
+    @Override
+    boolean isTarget(Menu menu) {
+        return menu.getCategory() == TARGET_CATEGORY;
     }
 
     @Override
