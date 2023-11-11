9fdeed09709baf60e8cd8fa5cbb66ef0d131cac2
seminchoi <choicco89@gmail.com>
2023-11-11 13:11:06 +0900

    feat(model.Orders) : 메뉴 목록을 읽을 수 있도록 Iterable 구현

--- src/main/java/christmas/model/Orders.java
+++ src/main/java/christmas/model/Orders.java
@@ -1,10 +1,17 @@
 package christmas.model;
 
 import java.util.EnumMap;
+import java.util.Iterator;
+import java.util.Map;
 
-public abstract class Orders {
+public abstract class Orders implements Iterable<Menu> {
 
     protected final EnumMap<Menu, Integer> orderMenus = new EnumMap<>(Menu.class);
+
+    @Override
+    public Iterator<Menu> iterator() {
+        return orderMenus.keySet().stream().iterator();
+    }
 
     public void addOrder(Menu menu, int count) {
         orderMenus.put(menu, count);
