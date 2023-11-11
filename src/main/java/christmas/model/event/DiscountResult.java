b9df472b08fd44587673db358ac3a6aff422e321
seminchoi <choicco89@gmail.com>
2023-11-11 18:34:43 +0900

    feat(model.event.DiscountResult) : 결과를 추가하는 기능 구현

--- src/main/java/christmas/model/event/DiscountResult.java
+++ src/main/java/christmas/model/event/DiscountResult.java
@@ -5,4 +5,8 @@ import java.util.Map;
 
 public class DiscountResult {
     private final Map<EventType, Integer> result = new EnumMap<>(EventType.class);
+
+    public void addResult(EventType eventType, int discountAmount) {
+        result.put(eventType, discountAmount);
+    }
 }
