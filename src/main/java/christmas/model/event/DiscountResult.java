9c7513a1ac92b7fe7a66b4263d2f96d9769f2dc7
seminchoi <choicco89@gmail.com>
2023-11-12 22:33:15 +0900

    feat(model.event.DiscountResult) : 총 할인 금액을 계산하는 기능 구현

--- src/main/java/christmas/model/event/DiscountResult.java
+++ src/main/java/christmas/model/event/DiscountResult.java
@@ -9,4 +9,12 @@ public class DiscountResult {
     public void addResult(EventKind eventKind, int discountAmount) {
         result.put(eventKind, discountAmount);
     }
+
+    public int sumTotalDiscountAmount() {
+        int totalDiscountAmount = 0;
+        for (Integer discountAmount : result.values()) {
+            totalDiscountAmount += discountAmount;
+        }
+        return totalDiscountAmount;
+    }
 }
