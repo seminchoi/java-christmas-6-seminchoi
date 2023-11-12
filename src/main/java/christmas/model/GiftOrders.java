30dc04840f43b3e15d227b4ad4f1d9c4af51f30c
seminchoi <choicco89@gmail.com>
2023-11-12 22:36:29 +0900

    feat(model.GiftOrders) : 증정 메뉴 금액을 총합하는 기능 구현

--- src/main/java/christmas/model/GiftOrders.java
+++ src/main/java/christmas/model/GiftOrders.java
@@ -1,4 +1,12 @@
 package christmas.model;
 
 public class GiftOrders extends Orders {
+    public int sumTotalGiftMenusPrice() {
+        int totalGiftMenusPrice = 0;
+        for (Menu menu : orderMenus.keySet()) {
+            final int giftCount = orderMenus.get(menu);
+            totalGiftMenusPrice += menu.getPrice() * giftCount;
+        }
+        return totalGiftMenusPrice;
+    }
 }
