8c119c4daf87752d61d34b4fc25238af646ae083
seminchoi <choicco89@gmail.com>
2023-11-13 12:23:04 +0900

    feat(model.OrderMenu) : 주문 메뉴와 개수를 저장하는 OrderMenu 클래스 생성

---
+++ src/main/java/christmas/model/OrderMenu.java
@@ -0,0 +1,11 @@
+package christmas.model;
+
+public class OrderMenu {
+    private final Menu menu;
+    private final int count;
+
+    public OrderMenu(final Menu menu, final int count) {
+        this.menu = menu;
+        this.count = count;
+    }
+}
