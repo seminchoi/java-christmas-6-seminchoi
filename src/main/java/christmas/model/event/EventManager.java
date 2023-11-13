e83641c81e76a947b71443b4cdb1c0b343bc0eb3
seminchoi <choicco89@gmail.com>
2023-11-13 22:14:48 +0900

    feat(model.EventManager) : 모든 이벤트 적용 기능 구현

--- src/main/java/christmas/model/event/EventManager.java
+++ src/main/java/christmas/model/event/EventManager.java
@@ -1,17 +1,19 @@
 package christmas.model.event;
 
+import christmas.model.EventPlanner;
+
 import java.util.PriorityQueue;
 
 public class EventManager {
     private final PriorityQueue<Event> events = new PriorityQueue<>();
 
-    public EventManager addEvent(Event event) {
+    public EventManager addEvent(final Event event) {
         validate(event);
         events.add(event);
         return this;
     }
 
-    private void validate(Event event) {
+    private void validate(final Event event) {
         for (Event existingEvent : events) {
             if (isAlreadySetEvent(existingEvent, event)) {
                 throw new IllegalArgumentException();
@@ -19,7 +21,13 @@ public class EventManager {
         }
     }
 
-    private boolean isAlreadySetEvent(Event existingEvent, Event event) {
+    private boolean isAlreadySetEvent(final Event existingEvent, final Event event) {
         return existingEvent.getClass().equals(event.getClass());
+    }
+
+    public void applyEvents(final EventPlanner eventPlanner) {
+        for (Event event : events) {
+            event.apply(eventPlanner);
+        }
     }
 }
