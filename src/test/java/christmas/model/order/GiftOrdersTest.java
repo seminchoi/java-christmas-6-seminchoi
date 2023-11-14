9b89748617414a2d67cc7566930d7b85eac54034
seminchoi <choicco89@gmail.com>
2023-11-14 18:06:24 +0900

    test(model.order.GiftOrderTest) : GiftOrders에 대한 테스트 코드 작성

---
+++ src/test/java/christmas/model/order/GiftOrdersTest.java
@@ -0,0 +1,26 @@
+package christmas.model.order;
+
+import static christmas.model.menu.Menu.CHAMPAGNE;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import christmas.model.menu.Menu;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.util.Map;
+
+class GiftOrdersTest {
+
+    @DisplayName("증정 주문을 추가한다.")
+    @Test
+    void given_order_when_addOrder_then_addedOrder() {
+        final GiftOrders giftOrders = new GiftOrders();
+        final Menu menu = CHAMPAGNE;
+        final int count = 1;
+
+        giftOrders.addOrder(menu, count);
+        Map<Menu, Integer> orders = giftOrders.getOrders();
+        final int actualCount = orders.get(menu);
+
+        assertThat(actualCount).isEqualTo(1);
+    }
+}
