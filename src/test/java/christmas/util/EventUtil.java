d4417dd3f929de2fc9c17096bda758ad1e6e07af
seminchoi <choicco89@gmail.com>
2023-11-14 19:17:42 +0900

    test(util.EventUtil) : 이벤트를 적용하고 결과를 반환하는 기능 구현

--- src/test/java/christmas/util/EventUtil.java
+++ src/test/java/christmas/util/EventUtil.java
@@ -1,10 +1,11 @@
 package christmas.util;
 
 import christmas.model.calendar.DecemberDate;
+import christmas.model.event.EventKind;
+import christmas.model.event.applicable.Event;
+import christmas.model.event.result.Benefits;
 import christmas.model.order.ClientOrders;
 import christmas.model.planner.EventPlanner;
-
-import java.util.HashMap;
 import java.util.Map;
 
 public class EventUtil {
@@ -13,5 +14,18 @@ public class EventUtil {
         final ClientOrders clientOrders = new ClientOrders(orders);
 
         return new EventPlanner(visitPlanDate, clientOrders);
+    }
+
+    public static int applyAndGetDiscountAmount(final Event event, final EventPlanner eventPlanner) {
+        Map<EventKind, Integer> benefitResult = applyAndGetBenefitResult(event, eventPlanner);
+        return benefitResult.get(event.getKind());
+    }
+
+    public static Map<EventKind, Integer> applyAndGetBenefitResult(
+            final Event event, final EventPlanner eventPlanner
+    ) {
+        event.apply(eventPlanner);
+        Benefits benefits = eventPlanner.getBenefits();
+        return benefits.getResult();
     }
 }
