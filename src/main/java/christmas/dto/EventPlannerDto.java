adf829fc50d31d9d9ce05f6d18ddc500a4ed9d3d
seminchoi <choicco89@gmail.com>
2023-11-15 00:01:48 +0900

    feat(dto.EventPlannerDto) : 레코드에서 클래스로 변경, 빌더 패턴 구현

--- src/main/java/christmas/dto/EventPlannerDto.java
+++ src/main/java/christmas/dto/EventPlannerDto.java
@@ -1,29 +1,129 @@
 package christmas.dto;
 
+import christmas.model.event.badge.Badge;
+import christmas.model.event.result.Benefits;
+import christmas.model.order.ClientOrders;
+import christmas.model.order.GiftOrders;
 import christmas.model.planner.EventPlanner;
 
-public record EventPlannerDto(
-        int date,
-        int amountBeforeDiscount,
-        OrdersDto clientOrders,
-        OrdersDto giftOrders,
-        BenefitsDto benefits,
-        int benefitAmount,
-        int finalAmount,
-        BadgeDto badge
-) {
-    public static EventPlannerDto of(final EventPlanner eventPlanner) {
-        int date = eventPlanner.getVisitPlanDate().getDate();
-        int amountBeforeDiscount = eventPlanner.getTotalAmountBeforeDiscount();
-        OrdersDto clientOrders = OrdersDto.of(eventPlanner.getClientOrders());
-        OrdersDto giftOrders = OrdersDto.of(eventPlanner.getGiftOrders());
-        BenefitsDto benefits = BenefitsDto.of(eventPlanner.getBenefits());
-        int benefitAmount = eventPlanner.getTotalBenefitAmount();
-        int finalAmount = eventPlanner.getFinalAmount();
-        BadgeDto badgeDto = BadgeDto.of(eventPlanner.getBadge());
+public class EventPlannerDto {
+    private int date;
+    private int amountBeforeDiscount;
+    private OrdersDto clientOrders;
+    private OrdersDto giftOrders;
+    private BenefitsDto benefits;
+    private int benefitAmount;
+    private int finalAmount;
+    private BadgeDto badge;
 
-        return new EventPlannerDto(
-                date, amountBeforeDiscount, clientOrders, giftOrders, benefits, benefitAmount, finalAmount, badgeDto
-        );
+    public static EventPlannerDto of(final EventPlanner eventPlanner) {
+        return new Builder()
+                .date(eventPlanner.getVisitPlanDate().getDate())
+                .amountBeforeDiscount(eventPlanner.getTotalAmountBeforeDiscount())
+                .clientOrders(eventPlanner.getClientOrders())
+                .giftOrders(eventPlanner.getGiftOrders())
+                .benefits(eventPlanner.getBenefits())
+                .benefitAmount(eventPlanner.getTotalBenefitAmount())
+                .finalAmount(eventPlanner.getFinalAmount())
+                .badge(eventPlanner.getBadge())
+                .build();
+    }
+
+    public EventPlannerDto (final Builder builder) {
+        this.date = builder.date;
+        this.amountBeforeDiscount = builder.amountBeforeDiscount;
+        this.clientOrders = builder.clientOrders;
+        this.giftOrders = builder.giftOrders;
+        this.benefits = builder.benefits;
+        this.benefitAmount = builder.benefitAmount;
+        this.finalAmount = builder.finalAmount;
+        this.badge = builder.badge;
+    }
+
+    public static class Builder {
+        private int date;
+        private int amountBeforeDiscount;
+        private OrdersDto clientOrders;
+        private OrdersDto giftOrders;
+        private BenefitsDto benefits;
+        private int benefitAmount;
+        private int finalAmount;
+        private BadgeDto badge;
+
+        public Builder date(final int date) {
+            this.date = date;
+            return this;
+        }
+
+        public Builder amountBeforeDiscount(final int amountBeforeDiscount) {
+            this.amountBeforeDiscount = amountBeforeDiscount;
+            return this;
+        }
+
+        public Builder clientOrders(final ClientOrders clientOrders) {
+            this.clientOrders = OrdersDto.of(clientOrders);
+            return this;
+        }
+
+        public Builder giftOrders(final GiftOrders giftOrders) {
+            this.giftOrders = OrdersDto.of(giftOrders);
+            return this;
+        }
+
+        public Builder benefits(final Benefits benefits) {
+            this.benefits = BenefitsDto.of(benefits);
+            return this;
+        }
+
+        public Builder benefitAmount(final int benefitAmount) {
+            this.benefitAmount = benefitAmount;
+            return this;
+        }
+
+        public Builder finalAmount(final int finalAmount) {
+            this.finalAmount = finalAmount;
+            return this;
+        }
+
+        public Builder badge(final Badge badge) {
+            this.badge = BadgeDto.of(badge);
+            return this;
+        }
+
+        public EventPlannerDto build() {
+            return new EventPlannerDto(this);
+        }
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public int getAmountBeforeDiscount() {
+        return amountBeforeDiscount;
+    }
+
+    public OrdersDto getClientOrders() {
+        return clientOrders;
+    }
+
+    public OrdersDto getGiftOrders() {
+        return giftOrders;
+    }
+
+    public BenefitsDto getBenefits() {
+        return benefits;
+    }
+
+    public int getBenefitAmount() {
+        return benefitAmount;
+    }
+
+    public int getFinalAmount() {
+        return finalAmount;
+    }
+
+    public BadgeDto getBadge() {
+        return badge;
     }
 }
