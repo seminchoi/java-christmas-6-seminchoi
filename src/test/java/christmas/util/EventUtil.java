32127fd7b589036b2f142472790e5e9d85e144cc
seminchoi <choicco89@gmail.com>
2023-11-14 20:23:28 +0900

    test(util.EventUtil) : 이벤트를 적용하고 증정 메뉴 목록을 반환하는 기능 구현

--- src/test/java/christmas/util/EventUtil.java
+++ src/test/java/christmas/util/EventUtil.java
@@ -4,7 +4,9 @@ import christmas.model.calendar.DecemberDate;
 import christmas.model.event.EventKind;
 import christmas.model.event.applicable.Event;
 import christmas.model.event.result.Benefits;
+import christmas.model.menu.Menu;
 import christmas.model.order.ClientOrders;
+import christmas.model.order.GiftOrders;
 import christmas.model.planner.EventPlanner;
 import java.util.Map;
 
@@ -27,5 +29,11 @@ public class EventUtil {
         event.apply(eventPlanner);
         Benefits benefits = eventPlanner.getBenefits();
         return benefits.getResult();
+    }
+
+    public static Map<Menu, Integer> applyAndGetGiftOrders(final Event event, final EventPlanner eventPlanner) {
+        event.apply(eventPlanner);
+        GiftOrders giftOrders = eventPlanner.getGiftOrders();
+        return giftOrders.getOrders();
     }
 }
