66f8bfaf2f569468eab9f2a6b99e40211e2aad73
seminchoi <choicco89@gmail.com>
2023-11-14 23:12:29 +0900

    feat(dto.BadgeDto) : toString 오버라이드 구현

--- src/main/java/christmas/dto/BadgeDto.java
+++ src/main/java/christmas/dto/BadgeDto.java
@@ -1,5 +1,6 @@
 package christmas.dto;
 
+import christmas.message.TitleMessage;
 import christmas.model.event.badge.Badge;
 
 public record BadgeDto(String eventKind, String badgeName) {
@@ -7,5 +8,11 @@ public record BadgeDto(String eventKind, String badgeName) {
         final String eventKind = badge.getEventKind().getDescription();
         final String badgeName = badge.getDescription();
         return new BadgeDto(eventKind, badgeName);
+    }
+
+    @Override
+    public String toString() {
+        final String title = String.format(TitleMessage.getTitleFormat(), eventKind);
+        return title + badgeName + "\n";
     }
 }
