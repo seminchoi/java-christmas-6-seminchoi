3ff6a5d9749694ccd5a011871470b4d0eb557822
seminchoi <choicco89@gmail.com>
2023-11-14 12:44:04 +0900

    feat(model.ClientOrders) : 단독 주문 불가능한 메뉴를 검증하는 기능 구현

--- src/main/java/christmas/model/ClientOrders.java
+++ src/main/java/christmas/model/ClientOrders.java
@@ -5,6 +5,7 @@ import static christmas.message.ErrorMessage.INVALID_ORDERS;
 import java.util.Map;
 
 public class ClientOrders extends Orders {
+    private final static int MIN_COUNT = 1;
     private final static int MAX_COUNT = 20;
 
     public ClientOrders(final Map<String, Integer> orderMenus) {
@@ -17,8 +18,13 @@ public class ClientOrders extends Orders {
     }
 
     private void validate(final Map<String, Integer> orderMenus) {
+        validateTotalCount(orderMenus);
+        validateMenus(orderMenus);
+    }
+
+    private void validateTotalCount(final Map<String, Integer> orderMenus) {
         int totalCount = sumTotalCount(orderMenus);
-        if (isOverTotalCount(totalCount)) {
+        if (isOutOfRangeTotalCount(totalCount)) {
             throw new IllegalArgumentException(INVALID_ORDERS.getMessage());
         }
     }
@@ -31,7 +37,28 @@ public class ClientOrders extends Orders {
         return totalCount;
     }
 
-    private boolean isOverTotalCount(final int totalCount) {
-        return totalCount > MAX_COUNT;
+    private boolean isOutOfRangeTotalCount(final int totalCount) {
+        return totalCount < MIN_COUNT || totalCount > MAX_COUNT;
+    }
+
+
+    private void validateMenus(final Map<String, Integer> orderMenus) {
+        if(isInvalidMenus(orderMenus)) {
+            throw new IllegalArgumentException(INVALID_ORDERS.getMessage());
+        }
+    }
+
+    private boolean isInvalidMenus(final Map<String, Integer> orderMenus) {
+        for (String menuName : orderMenus.keySet()) {
+            Menu menu = Menu.getMenuByName(menuName);
+            if(canOrderIndividually(menu)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean canOrderIndividually(final Menu menu) {
+        return menu.getCategory() != MenuCategory.BEVERAGE;
     }
 }
