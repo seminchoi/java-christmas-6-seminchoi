3ec58282af7779bffc5686e110af7001e3294482
seminchoi <choicco89@gmail.com>
2023-11-15 11:18:28 +0900

    refactor(model.event.badge.DecemberBadge) : 상수 선언 순서에 상관 없도록 비교 기능 구현

--- src/main/java/christmas/model/event/badge/DecemberBadge.java
+++ src/main/java/christmas/model/event/badge/DecemberBadge.java
@@ -5,32 +5,41 @@ import static christmas.model.event.EventKind.DECEMBER_BADGE;
 import christmas.model.event.EventKind;
 
 public enum DecemberBadge implements Badge {
-    SANTA("산타", -20_000),
-    TREE("트리", -10_000),
+    NOTHING("없음", 0),
     STAR("별", -5_000),
-    NOTHING("없음", 0)
+    TREE("트리", -10_000),
+    SANTA("산타", -20_000),
     ;
 
     private static final EventKind eventKind = DECEMBER_BADGE;
-    private final String description;
-    private final int minBenefitAmountForReceiving;
 
-    DecemberBadge(final String description, final int minBenefitAmountForReceiving) {
+    private final String description;
+    private final int benefitAmountForReceiving;
+
+    DecemberBadge(final String description, final int benefitAmountForReceiving) {
         this.description = description;
-        this.minBenefitAmountForReceiving = minBenefitAmountForReceiving;
+        this.benefitAmountForReceiving = benefitAmountForReceiving;
     }
 
     public static DecemberBadge findByBenefitAmount(final int benefitAmount) {
-        for (DecemberBadge badge : DecemberBadge.values()) {
+        DecemberBadge highestBadge = NOTHING;
+        for (final DecemberBadge badge : DecemberBadge.values()) {
             if(isSatisfiedEventCondition(badge, benefitAmount)) {
-                return badge;
+                highestBadge = highest(highestBadge, badge);
             }
         }
-        return NOTHING;
+        return highestBadge;
     }
 
     private static boolean isSatisfiedEventCondition(final DecemberBadge badge, final int benefitAmount) {
-        return badge.minBenefitAmountForReceiving >= benefitAmount;
+        return badge.benefitAmountForReceiving >= benefitAmount;
+    }
+
+    private static DecemberBadge highest(final DecemberBadge badge1, final DecemberBadge badge2) {
+        if(badge1.benefitAmountForReceiving < badge2.benefitAmountForReceiving) {
+            return badge1;
+        }
+        return badge2;
     }
 
     @Override
