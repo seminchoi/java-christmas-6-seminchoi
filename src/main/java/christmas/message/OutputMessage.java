551a9896f0ceb586dddc7254a8685e201cff925b
seminchoi <choicco89@gmail.com>
2023-11-15 11:22:00 +0900

    style(message.OutputMessage) : 가독성 향상을 위한 문자열 연산 수정

--- src/main/java/christmas/message/OutputMessage.java
+++ src/main/java/christmas/message/OutputMessage.java
@@ -7,7 +7,7 @@ public enum OutputMessage {
     VISIT_PLAN_DATE_FORMAT("12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!"),
     AMOUNT_FORMAT("%,d" + WON.message),
     ORDER_FORMAT("%s %d개"),
-    BENEFIT_FORMAT("%s: " + AMOUNT_FORMAT.message),
+    BENEFIT_FORMAT("%s: %,d" + WON.message),
     ;
 
     private final String message;
